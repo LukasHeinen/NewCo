0dc766ab4ef7d5f57349815c0fc85ebadd5f03d2
Paul.Hornberger <paul.hornberger@bmw.de>
2020-04-12 22:11:11 +0200

    Add circle click and display in creation screen, try to add pinch to zoom

--- clicky-clicky/ClientApp/src/app/app.module.ts
+++ clicky-clicky/ClientApp/src/app/app.module.ts
@@ -1,25 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
+import { PinchZoomModule } from "ngx-pinch-zoom";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    PinchZoomModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- clicky-clicky/ClientApp/src/app/tab2/tab2.page.ts
+++ clicky-clicky/ClientApp/src/app/tab2/tab2.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
-
+  displayRing = false;
   constructor() {}
 
+  getRelativePositionInPercent(event: MouseEvent) {
+    this.displayRing = true;
+    const target = event.target as HTMLTextAreaElement;
+    const rect = target.getBoundingClientRect();
+    const xPercent = (event.clientX - rect.left) / rect.width;
+    const yPercent = (event.clientY - rect.top) / rect.height;
+    console.log(xPercent, yPercent);
+    const root = document.documentElement;
+    root.style.setProperty("--circle-x", event.clientX - rect.left + "px");
+    root.style.setProperty("--circle-y", event.clientY - rect.top + "px");
+  }
 }
