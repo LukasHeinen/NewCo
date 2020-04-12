3a851cb06d1d4bfbe6c587b80958e0aa917ddbe7
SimonPfeiffer <pfeiffersimon@outlook.com>
2020-04-12 15:05:07 +0200

    Enable single page application routing

--- clicky-clicky/Startup.cs
+++ clicky-clicky/Startup.cs
@@ -108,19 +108,19 @@ namespace clicky_clicky
                     pattern: "{controller}/{action=Index}/{id?}");
             });
 
-            //app.UseSpa(spa =>
-            //{
-            //    // To learn more about options for serving an Angular SPA from ASP.NET Core,
-            //    // see https://go.microsoft.com/fwlink/?linkid=864501
-//
-            //    spa.Options.SourcePath = "ClientApp";
-//
-            //    if (env.IsDevelopment())
-            //    {
-            //        spa.UseAngularCliServer(npmScript: "start");
-            //        //spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
-            //    }
-            //});
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                    //spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                }
+            });
         }
     }
 }
