011d8bddc74d2b310e0db2e6b6730c960e8cc7bd
SimonPfeiffer <pfeiffersimon@outlook.com>
2020-04-12 15:03:12 +0200

    Add authorization for weatherforecast endpoint

--- clicky-clicky/Controllers/WeatherForecastController.cs
+++ clicky-clicky/Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -23,6 +24,7 @@ namespace clicky_clicky.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
