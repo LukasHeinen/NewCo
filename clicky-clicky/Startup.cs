f090d4936f46e5fb5d5c0f1e25e52cf25da4fc89
SimonPfeiffer <pfeiffersimon@outlook.com>
2020-04-12 15:02:56 +0200

    Add initial api authentication

--- clicky-clicky/Startup.cs
+++ clicky-clicky/Startup.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace clicky_clicky
 {
@@ -27,6 +31,36 @@ namespace clicky_clicky
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddDbContext<IdentityDbContext>(options =>
+                        options.UseInMemoryDatabase(databaseName: "Accounts"));
+            //options.UseSqlite("Data Source=users.sqlite",
+            //sqliteOptions => sqliteOptions.MigrationsAssembly("GoogleSpaWeb")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                    .AddEntityFrameworkStores<IdentityDbContext>()
+                    .AddDefaultTokenProviders();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultSignOutScheme = IdentityConstants.ApplicationScheme;
+            })
+            .AddGoogle(options =>
+            {
+                options.ClientId = "181801142749-eur7nnjh3kj3a4ut9p0ts2oro5gf867g.apps.googleusercontent.com";
+                options.ClientSecret = "azWmt3NyzOYyy1E65YZ9_y4-";
+            });
+            //.AddOpenIdConnect("Google", o =>
+            //{
+            //    o.ClientId = "181801142749-eur7nnjh3kj3a4ut9p0ts2oro5gf867g.apps.googleusercontent.com";
+            //    o.ClientSecret = "azWmt3NyzOYyy1E65YZ9_y4-";
+            //    o.Authority = "https://accounts.google.com";
+            //    o.ResponseType = OpenIdConnectResponseType.Code;
+            //    o.CallbackPath = "/signin-google"; // Or register the default "/sigin-oidc"
+            //    o.Scope.Add("email");
+            //});
+            //JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -54,14 +88,18 @@ namespace clicky_clicky
                 app.UseHsts();
             }
 
+            app.UseRouting();
+
+            app.UseAuthentication();
+            
+            app.UseAuthorization();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
                 app.UseSpaStaticFiles();
             }
-
-            app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
@@ -70,19 +108,19 @@ namespace clicky_clicky
                     pattern: "{controller}/{action=Index}/{id?}");
             });
 
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                    //spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
-                }
-            });
+            //app.UseSpa(spa =>
+            //{
+            //    // To learn more about options for serving an Angular SPA from ASP.NET Core,
+            //    // see https://go.microsoft.com/fwlink/?linkid=864501
+//
+            //    spa.Options.SourcePath = "ClientApp";
+//
+            //    if (env.IsDevelopment())
+            //    {
+            //        spa.UseAngularCliServer(npmScript: "start");
+            //        //spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+            //    }
+            //});
         }
     }
 }
